1ad3a87c19a1bea4d95664dd8ebb92d74700f325
Patrick <patrickrodrigues1239@gmail.com>
2025-03-18 23:32:45 -0300

    Adicionar cálculo para compras acima de 3000 reais e sua porcentagem do total

--- Desafio04/desafio04.py
+++ Desafio04/desafio04.py
@@ -5,4 +5,12 @@ media = sum(gastos)/quantidade
 print(f'A media de gastos e {media:.2f}')
 
 #Com os mesmos dados da questão anterior, defina quantas compras foram realizadas acima de 3000 reais e calcule a porcentagem quanto ao total de compras.
+maiores_compras = 0 
+for valores in gastos:
+    if valores > 3000:
+        maiores_compras += 1
 
+porcentagem = (maiores_compras/quantidade)*100
+print(f'Foi realizado {maiores_compras} compras acima de 3.000,00 reais, sendo {porcentagem:.2f}% da quantidade total de compras.')
+
+#Faça um código que colete em uma lista 5 números inteiros quaisquer e imprima a lista. Exemplo: [1,4,7,2,4].
