f46bc7f4316ae209311439558198110c88cfc2be
Patrick <patrickrodrigues1239@gmail.com>
2025-03-21 00:40:20 -0300

    Implementar cálculo de média de gastos e validação de datas

--- Desafio04/desafio04.py
+++ Desafio04/desafio04.py
@@ -134,3 +134,78 @@ for i in vendas.keys():
 print(f'O total de vendas: {total}')
 print(f'O produto mais vendido: {produto_mais_vendido}')
 
+# Uma pesquisa de mercado foi feita para decidir qual design de marca infantil mais agrada as crianças. A pesquisa foi feita e o votos computados podem ser observados abaixo:
+# Tabela de votos da marca
+# Design 1 - 1334 votos
+# Design 2 - 982 votos
+# Design 3 - 1751 votos
+# Design 4 - 210 votos
+# Design 5 - 1811 votos
+#Adapte os dados fornecidos para uma estrutura de dicionário. A partir dele, informe o design vencedor e a porcentagem de votos recebidos.
+votos = {'Design 1': 1334, 'Design 2':982, 'Design 3':1751, 'Design 4':210, 'Design 5': 1811}
+votos_total = 0
+desing = ''
+quntidade_vts = 0
+percentual = 0
+
+for i in votos.keys():
+    votos_total += votos[i]
+    if votos[i] > quntidade_vts:
+        quntidade_vts = votos[i]
+        desing = i
+        percentual = (quntidade_vts/votos_total)*100
+print(f'O {desing} venceu com {percentual:.2f}% de votos')
+
+# As pessoas colaboradoras de um setor da empresa que você trabalha vão receber um abono correspondente a 10% do salário devido ao ótimo desempenho do time. O setor financeiro solicitou sua ajuda para a verificação das consequências financeiras que esse abono irá gerar nos recursos. Assim, foi encaminhada para você uma lista com os salários que receberão o abono: [1172, 1644, 2617, 5130, 5532, 6341, 6650, 7238, 7685, 7782, 7903]. O abono de cada colaborador(a) não pode ser inferior a 200. Em código, transforme cada um dos salários em chaves de um dicionário e o abono de cada salário no elemento. Depois, informe o total de gastos com o abono, quantos(as) colaboradores(as) receberam o abono mínimo e qual o maior valor de abono fornecido.
+salarios = [1172, 1644, 2617, 5130, 5532, 6341, 6650, 7238, 7685, 7782, 7903]
+abonos = {salario:max(salario*0.1,200) for salario in salarios}
+total_abono = sum(abonos.values())
+abono_minimo = sum(1 for abono in abonos.values() if abono == 200)
+maior_abono = max(abonos.values())
+
+print("Dicionário de abonos:", abonos)
+print(f'Total gasto com abonos: {total_abono}')
+print(f'Colaboradores que receberam o abono mínimo: {abono_minimo:.2f}')
+print(f'Maior valor de abono fornecido: {maior_abono:.2f}')
+
+# Uma equipe de cientistas de dados está estudando a diversidade biológica em uma floresta. A equipe fez a coleta de informações sobre o número de espécies de plantas e animais em cada área dessa floresta e armazenou essas informações em um dicionário. Nele, a chave descreve a área dos dados e os valores nas listas correspondem às espécies de plantas e animais nas áreas, respectivamente.
+# {'Área Norte': [2819, 7236],
+#  'Área Leste': [1440, 9492],
+#  'Área Sul': [5969, 7496],
+#  'Área Oeste': [14446, 49688],
+#  'Área Centro': [22558, 45148]}
+# Copiar código
+# Escreva um código para calcular a média de espécies por área e identificar a área com a maior diversidade biológica. Dica: use as funções built-in sum() e len().
+areas = {'Área Norte': [2819, 7236], 'Área Leste': [1440, 9492], 'Área Sul': [5969, 7496], 'Área Oeste': [14446, 49688], 'Área Centro': [22558, 45148]}
+
+medias = {area:sum(especies)/len(especies) for area, especies in areas.items()}
+maior_diversidade = max(medias, key=medias.get)
+
+print(f'Media de espécie por area: {medias}\n Área com maior divercidade: {maior_diversidade}')
+
+# O setor de RH da sua empresa te pediu uma ajuda para analisar as idades de colaboradores(as) de 4 setores da empresa. Para isso, foram fornecidos os seguintes dados:
+
+# {'Setor A': [22, 26, 30, 30, 35, 38, 40, 56, 57, 65],
+#  'Setor B': [22, 24, 26, 33, 41, 49, 50, 54, 60, 64],
+#  'Setor C': [23, 26, 26, 29, 34, 35, 36, 41, 52, 56],
+#  'Setor D': [19, 20, 25, 27, 34, 39, 42, 44, 50, 65]}
+# Copiar código
+# Sabendo que cada setor tem 10 colaboradores(as), construa um código que calcule a média de idade de cada setor, a idade média geral entre todos os setores e quantas pessoas estão acima da idade média geral.
+dados = {
+    'Setor A': [22, 26, 30, 30, 35, 38, 40, 56, 57, 65],
+    'Setor B': [22, 24, 26, 33, 41, 49, 50, 54, 60, 64],
+    'Setor C': [23, 26, 26, 29, 34, 35, 36, 41, 52, 56],
+    'Setor D': [19, 20, 25, 27, 34, 39, 42, 44, 50, 65]
+}
+
+medias_setores = {setor: sum(idades) / len(idades) for setor, idades in dados.items()}
+idades_geral = [idade for idades in dados.values() for idade in idades]
+media_geral = sum(idades_geral)/ len(idades_geral)
+acima = sum(1 for idade in idades_geral if idade > media_geral)
+
+print("Média de idade por setor:")
+for setor, media in medias_setores.items():
+    print(f"{setor}: {media:.2f} anos")
+
+print(f"\nMédia de idade geral: {media_geral:.2f} anos")
+print(f"Número de colaboradores acima da média geral: {acima}")
